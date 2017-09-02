e2b19319c75066bfa7e95b5b11254767f5a9318b
jayk <jayk.inno@outlook.com>
2017-09-02 13:32:19 +0200

    feature(home): add home page and error page route/

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,18 +8,23 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from "./login/login-form.component";
 import { RegisterFormComponent } from "./register/register-form.component";
+import { ErrorComponent } from "./404/error.component";
+import { HomePageComponent } from "./homePage/home-page.component";
 
 let appRoutes: Routes = [
   { path: 'register', component: RegisterFormComponent },
   { path: 'login', component: LoginFormComponent },
-  { path: '**', component: AppComponent }
+  { path: '', component: HomePageComponent }, 
+  { path: '**', component: ErrorComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
-    RegisterFormComponent
+    RegisterFormComponent,
+    ErrorComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
